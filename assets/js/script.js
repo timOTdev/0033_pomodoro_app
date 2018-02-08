470e39bd31508410807f6e9e61329b82d946de63
Timothy <timh1203@gmail.com>
2018-02-08 14:24:31 -0500

    Add countdown feature for session and break

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,30 +11,90 @@ let breakDecrease = document.getElementById("breakDecrease");
 
 let sessionCounter = 25;
 let breakCounter = 5;
+let sessionStartTime = sessionCounter * 60; // 1500
+let breakStartTime = breakCounter * 60; // 300
 
-sessionTime.append(sessionCounter);
-breakTime.append(breakCounter);
+sessionTime.append(sessionCounter + ":00");
+breakTime.append(breakCounter + ":00");
+
+// Click Listeners
+pomodoroTop.addEventListener("click", (e) => {
+  e.preventDefault;
+  let timer = sessionStartTime - 1;
+  let minutes;
+  let seconds;
+
+  let startSession = setInterval(function() {
+    if (timer === 0) {
+      clearInterval(startSession);
+      sessionTime = sessionTime.textContent = sessionCounter + ":00";
+    }
+
+    minutes = Math.floor(timer / 60);
+    seconds = (timer % 60);
+    
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+    timer--;
+    sessionTime.textContent = minutes + ":" + seconds;
+  }, 1000);
+})
+
+pomodoroBottom.addEventListener("click", (e) => {
+  e.preventDefault;
+  let timer = breakStartTime - 1;
+  let minutes;
+  let seconds;
+  let startBreak = setInterval(function() {
+    if (timer === 0) {
+      clearInterval(startBreak);
+      breakTime = breakTime.textContent = breakCounter + ":00";
+    }
+
+    minutes = Math.floor(timer / 60);
+    seconds = (timer % 60);
+    
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+    timer--;
+    breakTime.textContent = minutes + ":" + seconds;
+  }, 1000);
+})
 
 sessionIncrease.addEventListener("click", (e) => {
   e.preventDefault;
+  e.stopPropagation();
   sessionCounter += 1;
-  sessionTime.innerHTML = sessionCounter;
+  sessionStartTime = sessionCounter * 60;
+  sessionTime.textContent = sessionCounter + ":00";
+  console.log("Session time increased to:" + sessionCounter + " minutes, " + sessionStartTime + " seconds");
 });
 
 sessionDecrease.addEventListener("click", (e) => {
   e.preventDefault;
+  e.stopPropagation();
   sessionCounter -= 1;
-  sessionTime.innerHTML = sessionCounter;
+  sessionStartTime = sessionCounter * 60;
+  sessionTime.textContent = sessionCounter + ":00";
+  console.log("Session time decreased to:" + sessionCounter + " minutes, " + sessionStartTime + " seconds");
 });
 
 breakIncrease.addEventListener("click", (e) => {
   e.preventDefault;
+  e.stopPropagation();
   breakCounter += 1;
-  breakTime.innerHTML = breakCounter;
+  breakStartTime = breakCounter * 60;
+  breakTime.textContent = breakCounter + ":00";
+  console.log("Break time increased to:" + breakCounter + " minutes, " + breakStartTime + " seconds");
 });
 
 breakDecrease.addEventListener("click", (e) => {
   e.preventDefault;
+  e.stopPropagation();
   breakCounter -= 1;
-  breakTime.innerHTML = breakCounter;
+  breakStartTime = breakCounter * 60;
+  breakTime.textContent = breakCounter + ":00";
+  console.log("Break time decreased to:" + breakCounter + " minutes, " + breakStartTime + " seconds");
 });
